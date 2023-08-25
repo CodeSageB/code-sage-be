b73d3b3d0bdcccbc1c3dd1cb0f24eac907e7921a
Martin Koller <kollermartin00@gmail.com>
2023-08-25 20:22:11 +0200

    fix: unit tests

--- src/blogs/services/blogs/blogs.service.spec.ts
+++ src/blogs/services/blogs/blogs.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogsService } from './blogs.service';
+import { Repository } from 'typeorm';
+import { BlogEntity } from '../../../typeorm/entities/Blog.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('BlogsService', () => {
   let service: BlogsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BlogsService]
+      providers: [
+        BlogsService,
+        {
+          provide: getRepositoryToken(BlogEntity),
+          useClass: Repository
+        }
+      ]
     }).compile();
 
     service = module.get<BlogsService>(BlogsService);
