ef1b2f61cf3b47ca755b4f18d7b636f28212d4a3
Martin Koller <kollermartin00@gmail.com>
2023-11-19 13:06:03 +0100

    fix: lint

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,12 +1,24 @@
 import { registerAs } from '@nestjs/config';
-import {readFileSync} from 'fs';
-import {join} from 'path';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 export default registerAs('database', () => {
-
   const isProduction = process.env.NODE_ENV === 'production';
 
-  let databaseConfig: {type: string, host: string, port: number, username: string, password: string, database: string, entities: string[], synchronize: boolean, logging: boolean, migrations: string[], migrationsTableName: string, ssl?: any} = {
+  let databaseConfig: {
+    type: string;
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    database: string;
+    entities: string[];
+    synchronize: boolean;
+    logging: boolean;
+    migrations: string[];
+    migrationsTableName: string;
+    ssl?: any;
+  } = {
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT, 10) || 35432,
@@ -17,16 +29,18 @@ export default registerAs('database', () => {
     synchronize: !isProduction,
     logging: !isProduction,
     migrations: [`${__dirname}/../../db/migrations/*{.ts,.js}`],
-    migrationsTableName: 'migrations',
+    migrationsTableName: 'migrations'
   };
 
   if (isProduction) {
     databaseConfig = {
-        ...databaseConfig,
+      ...databaseConfig,
       ssl: {
-        ca: readFileSync(join(__dirname, '..', '..', 'assets', 'eu-north-1-bundle.pem')).toString()
-      },
-    }
+        ca: readFileSync(
+          join(__dirname, '..', '..', 'assets', 'eu-north-1-bundle.pem')
+        ).toString()
+      }
+    };
   }
 
   return databaseConfig;
